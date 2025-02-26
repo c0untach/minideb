f6832896ecb8a3552a2d20f6af3b8c855806dae6
c0untach <51246376+c0untach@users.noreply.github.com>
2025-02-26 10:23:32 +0200

    Update configurate.sh

--- scripts/configurate.sh
+++ scripts/configurate.sh
@@ -7,6 +7,10 @@ mkdir -p /home/user/.local/share/desktop-directories
 mkdir -p /home/user/.config/menus/applications-merged
 mkdir -p /home/user/.local/share/applications
 
+# populate configs
 cat configs/vbox.directory > /home/user/.local/share/desktop-directories/vbox.directory
 cat configs/vbox.menu > /home/user/.config/menus/applications-merged/vbox-directory
 cat configs/vbox.desktop > /home/user/.local/share/applications/vbox.desktop
+
+# populate panel config
+cat configs/xfce4-panel.xml > /home/user/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
