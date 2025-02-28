3e83dd818fb0b67ba06b1f26a30b74be809ff536
c0untach <51246376+c0untach@users.noreply.github.com>
2025-02-28 12:04:37 +0200

    Add files via upload

--- scripts/hardening/hardening.sh
+++ scripts/hardening/hardening.sh
@@ -131,8 +131,52 @@ function main {
     source "$s"
   done
 
+  f_pre
+  f_kernel
+  f_firewall
+  f_disablenet
+  f_disablefs
+  f_disablemod
+  f_systemdconf
+  f_logindconf
+  f_journalctl
+  f_fstab
+  f_prelink
+  f_hosts
   f_issue
-
+  f_sudo
+  f_logindefs
+  f_sysctl
+  f_limitsconf
+  f_adduser
+  f_rootaccess
+  f_package_install
+  f_psad
+  f_coredump
+  f_usbguard
+  f_postfix
+  f_motdnews
+  f_rkhunter
+  f_sshconfig
+  f_sshdconfig
+  f_cron
+  f_ctrlaltdel
+  f_auditd
+  f_aide
+  f_rhosts
+  f_users
+  f_lockroot
+  f_package_remove
+  f_suid
+  f_restrictcompilers
+  f_umask
+  f_path
+  f_aa_enforce
+  f_aide_post
+  f_aide_timer
+  f_systemddelta
+  f_post
+  f_checkreboot
 
   echo
 }
